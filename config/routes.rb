b9707fade7ed4922eff569a639a9844052f95e48
ichiroc <nobitahouse@gmail.com>
2018-12-30 15:16:05 +0900

    ログイン画面に不要な入力欄を表示しない

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users, controllers: {
-               omniauth_callbacks: 'users/omniauth_callbacks'
+               omniauth_callbacks: 'users/omniauth_callbacks',
+               sessions: 'users/sessions'
              }
   root to: 'visitors#index'
 
